ec5b8dba2211a0e4944f8a94d4bb40de9b78f9b6
limed <limed@mozilla.com>
2015-10-29 11:24:51 -0700

    Create symlink to associate eni in nat.pp

--- nubis/puppet/nat.pp
+++ nubis/puppet/nat.pp
@@ -16,6 +16,12 @@ file { '/usr/local/bin/eni-associate':
     require => File['/usr/local/lib/util.sh']
 }
 
+file { '/etc/nubis.d/98-eni-associate':
+    ensure  => link,
+    target  => '/usr/local/bin/eni-associate',
+    require => File['/usr/local/bin/eni-associate'],
+}
+
 file { '/usr/local/bin/nat.sh':
     ensure  => file,
     owner   => root,
