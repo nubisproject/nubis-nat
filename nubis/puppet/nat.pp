4dd58c52765d5c2e4c58f4c226f4a1ba5be791d6
limed <limed@mozilla.com>
2016-02-12 11:04:43 -0800

    Add advertise_addr config since NAT instances has 2 interfaces

--- nubis/puppet/nat.pp
+++ nubis/puppet/nat.pp
@@ -13,6 +13,15 @@ file { '/etc/nubis.d/98-eni-associate':
     require => File['/usr/local/bin/eni-associate'],
 }
 
+# This is 000 on purpose it needs to run before consul
+file { '/etc/nubis.d/000-interface-fixup':
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+    source => 'puppet:///nubis/files/interface-fixup',
+}
+
 class { 'nubis_nat':
     nat_in_interface  => 'eth1',
     nat_out_interface => 'eth0',
