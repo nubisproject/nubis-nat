7b12604fc38fe252a95ccedaed35f6a61c606a4c
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-02-24 20:41:30 -0500

    POC: Implement a VPC blocklisting process.
    Right now, some obvious places for improvement:
     - hard-coded blocklist URL (http://people.mozilla.org/~pchiasson/blocklist/blocklist.txt.asc)
     - hard-coded GPG KeyID (gozer's)
     - Self-contained, no Consul lookups/integrations
    
    But the short of it is that it works. I'll be filing followup issues to
    get it all fixed up.
    
    Fixes #23

---
+++ nubis/puppet/04_blocklist.pp
@@ -0,0 +1,19 @@
+file { "/usr/local/bin/nubis-nat-vpc-blocklist":
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0755',
+  source  => "puppet:///nubis/files/vpc-blocklist"
+}
+
+cron::job{ 'nubis-nat-vpc-blocklist':
+  minute      => '*',
+  hour        => '*',
+  date        => '*',
+  month       => '*',
+  weekday     => '*',
+  user        => 'root',
+  command     => '/usr/local/bin/nubis-nat-vpc-blocklist',
+  environment => [ 
+    'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"',
+  ]
+}
