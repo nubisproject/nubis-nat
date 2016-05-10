06af62fa8084be1cad085b5b9b238a981309a5f7
limed <limed@mozilla.com>
2016-05-10 12:35:06 -0700

    Enforce dependency

--- nubis/puppet/00_nat.pp
+++ nubis/puppet/00_nat.pp
@@ -70,10 +70,11 @@ file { '/etc/nubis.d/0-1-interface-fixup':
     source => 'puppet:///nubis/files/interface-fixup',
 }
 
+# TODO: Should have a proper fix and not have nsm class
+# install supervisor
 class { 'nsm':
     nsm_mailto  => 'infosec@mozilla.com'
-}
-
+}->
 class { 'nubis_nat':
     startup_order     => '0-3',
     nat_in_interface  => 'eth1',
